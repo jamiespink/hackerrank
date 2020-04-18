bf65c4fc42f8fdab6605652188462a297fafc300
Jamie Spink <jamiespink2@gmail.com>
2020-04-18 17:57:52 +0200

    breaking-the-records

---
+++ breaking-the-records.js
@@ -0,0 +1,17 @@
+function breakingRecords(scores) {
+    let high = scores[0];
+    let low = scores[0];
+    let highCount = 0;
+    let lowCount = 0;
+    for (let i = 1; i < scores.length; i++) {
+        if (scores[i] > high) {
+            high = scores[i];
+            highCount ++;
+        }
+        if (scores[i] < low) {
+            low = scores[i];
+            lowCount ++;
+        }
+    }
+    return [highCount, lowCount];
+}
