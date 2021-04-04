0ebd509cf81d96fe9c1d4fe2a04caab5e3ebd4d6
Jamie Spink <jamie.spink@qmmedia.se>
2021-04-04 16:13:15 +0200

    moved towards a more declarative style with reduce function

--- the-birthday-bar.js
+++ the-birthday-bar.js
@@ -1,15 +1,13 @@
 // https://www.hackerrank.com/challenges/the-birthday-bar/problem
 function birthday(s, d, m) {
-    let segmentSum = 0;
-    let segmentCount = 0;
-    for (var i = 0; i < s.length; i++) {
-        segmentSum = s[i];
-        for (var j = i + 1; j < i + m; j++) {
+    return s.reduce((count, currentValue, i) => {
+        let segmentSum = 0;
+        for (var j = i; j < i + m; j++) {
             segmentSum += s[j];
         }
         if (segmentSum === d) {
-            segmentCount ++;
+            return count + 1;
         }
-    }
-    return segmentCount;
+        return count;
+    }, 0)
 }
